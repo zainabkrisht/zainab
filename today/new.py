de8304efe3a903a957914776ab5d8801ca633fbb
zainabkrisht <144453270+zainabkrisht@users.noreply.github.com>
2023-09-21 17:23:31 +0300

    Add files via upload

---
+++ today/new.py
@@ -0,0 +1,19 @@
+print("hello world")
+if 5>2:
+    print("five is greter than two")
+
+x=8
+y="krisht"
+
+print(x,y)
+
+
+x,y,z = "black", "orange","green"
+print(x)
+print(y)
+print(z)
+
+age=1
+year=2024
+mysent="ivanna will be {} in {}"
+print(mysent.format(age,year)) 
